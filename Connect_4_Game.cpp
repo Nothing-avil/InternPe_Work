a387554d754ad4825242519d0818ccf1e6fd565a
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2023-09-18 09:39:54 +0530

    Create Connect_4_Game.cpp

---
+++ Connect_4_Game.cpp
@@ -0,0 +1,216 @@
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+using namespace std;
+
+struct playerInfo
+{
+	char playerName[81];
+	char playerID;
+};
+
+int PlayerDrop( char board[][10], playerInfo activePlayer )
+{
+	int dropChoice;
+	do
+	{
+		cout << activePlayer.playerName << "'s Turn ";
+		cout << "Please enter a number between 1 and 7: ";
+		cin  >> dropChoice;
+
+		while ( board[1][dropChoice] == 'X' || board[1][dropChoice] == 'O' )
+		{
+			cout << "That row is full, please enter a new row: ";
+			cin  >> dropChoice;
+		}
+
+	}while ( dropChoice < 1 || dropChoice > 7 );
+
+    return dropChoice;
+}
+
+void CheckBellow ( char board[][10], playerInfo activePlayer, int dropChoice )
+{
+	int length, turn;
+	length = 6;
+	turn = 0;
+
+	do 
+	{
+		if ( board[length][dropChoice] != 'X' && board[length][dropChoice] != 'O' )
+		{
+			board[length][dropChoice] = activePlayer.playerID;
+			turn = 1;
+		}
+		else
+		--length;
+	}while (  turn != 1 );
+}
+
+void DisplayBoard ( char board[][10] )
+{
+    system("clear");
+	for(int q = 0; q<9; q++)
+    {
+        if(q<2)
+        {
+        std::cout<<"-";
+        } else if(q>7)
+        {
+        std::cout<<q-1<<"--"<<std::endl;
+        } else {
+        std::cout<<q-1<<"----";
+        }
+    }
+    for(int i = 1; i<=6; i++)
+    {
+        for(int j = 1; j<=7; j++)
+        {
+            if(board[i][j] != 'X' && board[i][j] != 'O')
+				board[i][j] = '*';
+
+			cout<<"| " << board[i][j]<<" |";
+        } std::cout<<std::endl;
+        
+    }
+    for(int i = 0; i<35; i++)
+    {
+        std::cout<<"=";
+    } std::cout<<std::endl;
+}
+
+int CheckFour ( char board[][10], playerInfo activePlayer )
+{
+	char XO;
+	int win;
+	XO = activePlayer.playerID;
+	win = 0;
+
+	for( int i = 8; i >= 1; --i )
+	{
+		for( int ix = 9; ix >= 1; --ix )
+		{
+			if( board[i][ix] == XO && board[i-1][ix-1] == XO && board[i-2][ix-2] == XO && board[i-3][ix-3] == XO )
+			{
+				win = 1;
+			}
+
+			if( board[i][ix] == XO && board[i][ix-1] == XO && board[i][ix-2] == XO && board[i][ix-3] == XO )
+			{
+				win = 1;
+			}
+					
+			if( board[i][ix] == XO && board[i-1][ix] == XO && board[i-2][ix] == XO && board[i-3][ix] == XO )
+			{	
+				win = 1;
+			}
+					
+			if( board[i][ix] == XO && board[i-1][ix+1] == XO && board[i-2][ix+2] == XO && board[i-3][ix+3] == XO )
+			{
+				win = 1;
+			}
+						
+			if ( board[i][ix] == XO && board[i][ix+1] == XO && board[i][ix+2] == XO && board[i][ix+3] == XO )
+			{
+				win = 1;
+			}
+		}
+	}
+    return win;
+}
+
+int FullBoard( char board[][10] )
+{
+	int full;
+	full = 0;
+	for ( int i = 1; i <= 7; ++i )
+	{
+		if ( board[1][i] != '*' )
+			++full;
+	}
+    return full;
+}
+
+void PlayerWin ( playerInfo activePlayer )
+{
+	cout << endl << activePlayer.playerName << " Connected Four, You Win!" << endl;
+}
+
+int restart ( char board[][10] )
+{
+	int restart;
+	cout << "Would you like to restart? Yes(1) No(2): ";
+	cin  >> restart;
+	if ( restart == 1 )
+	{
+		for(int i = 1; i <= 6; i++)
+		{
+			for(int ix = 1; ix <= 7; ix++)
+			{
+				board[i][ix] = '*';
+			}
+		}
+	}
+	else
+		cout << "Goodbye!" << endl;
+    return restart;
+}
+int main()
+{
+	playerInfo playerOne, playerTwo;
+	char board[9][10]; 
+	int trueWidth = 7; 
+	int trueLength = 6; 
+	int dropChoice, win, full, again;
+
+	cout << "Let's Play Connect 4" << endl << endl;
+	cout << "Player One please enter your name: ";
+	cin  >> playerOne.playerName;
+	playerOne.playerID = 'X';
+	cout << "Player Two please enter your name: ";
+	cin  >> playerTwo.playerName;
+	playerTwo.playerID = 'O';
+	
+	full = 0;
+	win = 0;
+	again = 0;
+	sleep(1);
+	DisplayBoard( board );
+	do
+	{
+		dropChoice = PlayerDrop( board, playerOne );
+		CheckBellow( board, playerOne, dropChoice );
+		DisplayBoard( board );
+		win = CheckFour( board, playerOne );
+		if ( win == 1 )
+		{
+			PlayerWin(playerOne);
+			again = restart(board);
+			if (again == 2)
+			{
+				break;
+			}
+		}
+
+		dropChoice = PlayerDrop( board, playerTwo );
+		CheckBellow( board, playerTwo, dropChoice );
+		DisplayBoard( board );
+		win = CheckFour( board, playerTwo );
+		if ( win == 1 )
+		{
+			PlayerWin(playerTwo);
+			again = restart(board);
+			if (again == 2)
+			{
+				break;
+			}
+		}
+		full = FullBoard( board );
+		if ( full == 7 )
+		{
+			cout << "The board is full, it is a draw!" << endl;
+			again = restart(board);
+		}
+	}while ( again != 2 );
+    return 0;
+}
